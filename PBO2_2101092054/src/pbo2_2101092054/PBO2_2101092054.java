5a2ed616506f768f54d2a71fb574a105b287b808
MuhamadFaisal28 <114376862+MuhamadFaisal28@users.noreply.github.com>
2023-03-09 15:53:25 +0700

    Create PBO2_2101092054.java

---
+++ PBO2_2101092054/src/pbo2_2101092054/PBO2_2101092054.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_2101092054;
+
+import faisal.dao.Koneksi;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO2_2101092054 {
+    public static void main(String[]args) {
+        try {
+            Koneksi koneksi = new Koneksi();
+            Connection con = koneksi.getKoneksi();
+            JOptionPane.showMessageDialog(null, "Koneksi Ok");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error :" + ex.getMessage());
+            Logger.getLogger(PBO2_2101092054.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+    
+  
