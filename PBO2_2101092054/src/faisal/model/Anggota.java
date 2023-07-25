589c5683e06c0e3fccdb8d7f9562b2cb46ee640b
MuhamadFaisal28 <114376862+MuhamadFaisal28@users.noreply.github.com>
2023-07-25 19:10:20 +0700

    Update Anggota.java

--- PBO2_2101092054/src/faisal/model/Anggota.java
+++ PBO2_2101092054/src/faisal/model/Anggota.java
@@ -7,38 +7,40 @@ package faisal.model;
 
 /**
  *
- * @author ASUS
+ * @author User
  */
 public class Anggota {
-    private String kodeAnggota;
-    private String namaAnggota;
+    private String kodeanggota;
+    private String namaanggota;
     private String alamat;
-    private String jenisKelamin;
+    private String jeniskelamin;
     
-    public Anggota() {
+    public Anggota(){
+        
     }
 
-    public Anggota(String kodeAnggota, String namaAnggota, String alamat, String jenisKelamin) {
-        this.kodeAnggota = kodeAnggota;
-        this.namaAnggota = namaAnggota;
+    public Anggota(String kodeanggota, String namaanggota, String alamat, String jeniskelamin) {
+        this.kodeanggota = kodeanggota;
+        this.namaanggota = namaanggota;
         this.alamat = alamat;
-        this.jenisKelamin = jenisKelamin;
+        this.jeniskelamin = jeniskelamin;
     }
 
-    public String getKodeAnggota() {
-        return kodeAnggota;
+
+    public String getKodeanggota() {
+        return kodeanggota;
     }
 
-    public void setKodeAnggota(String kodeAnggota) {
-        this.kodeAnggota = kodeAnggota;
+    public void setKodeanggota(String kodeanggota) {
+        this.kodeanggota = kodeanggota;
     }
 
-    public String getNamaAnggota() {
-        return namaAnggota;
+    public String getNamaanggota() {
+        return namaanggota;
     }
 
-    public void setNamaAnggota(String namaAnggota) {
-        this.namaAnggota = namaAnggota;
+    public void setNamaanggota(String namaanggota) {
+        this.namaanggota = namaanggota;
     }
 
     public String getAlamat() {
@@ -49,13 +51,22 @@ public class Anggota {
         this.alamat = alamat;
     }
 
-    public String getJenisKelamin() {
-        return jenisKelamin;
+    public String getJeniskelamin() {
+        return jeniskelamin;
     }
 
-    public void setJenisKelamin(String jenisKelamin) {
-        this.jenisKelamin = jenisKelamin;
+    public void setJeniskelamin(String jeniskelamin) {
+        this.jeniskelamin = jeniskelamin;
     }
     
+    public static void main(String[] args) {
+        Anggota ali = new Anggota("A001","Faisal","Padang","L");
+        ali.setKodeanggota("001");
+        ali.kodeanggota="002";
+    
+        System.out.println("Kode    "+ali.getKodeanggota());
+    }
+
+   
     
 }
