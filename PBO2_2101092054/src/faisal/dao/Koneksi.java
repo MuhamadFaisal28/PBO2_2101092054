af6439cb6eb43b2165a17dd9f8e8530442d562ae
MuhamadFaisal28 <114376862+MuhamadFaisal28@users.noreply.github.com>
2023-07-25 19:10:44 +0700

    Update Koneksi.java

--- PBO2_2101092054/src/faisal/dao/Koneksi.java
+++ PBO2_2101092054/src/faisal/dao/Koneksi.java
@@ -4,23 +4,25 @@
  * and open the template in the editor.
  */
 package faisal.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 /**
  *
- * @author ASUS
+ * @author User
  */
 public class Koneksi {
-   private final String url ="jdbc:mysql://localhost/PBO2_2101092054" ;
-   private final String username ="root";
-   private final String password="";
-   
-   public Connection getKoneksi()throws ClassNotFoundException, SQLException{
-       Class.forName("com.mysql.jdbc.Driver");
-       return DriverManager.getConnection(url,username,password);
-   }
+    private String url = "jdbc:mysql://localhost/pbo2_2101092054";
+    private String username = "root";
+    private String password = "";
+    
+    public Connection getKoneksi() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(url, username, password);
+    }
+
    
 }
+
